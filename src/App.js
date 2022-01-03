bc11bc7eca7348c983bac4c0583eaddfc7426915
Diego <drbv27@gmail.com>
2022-01-03 08:45:48 -0500

    adding elemnts

--- src/App.js
+++ src/App.js
@@ -11,12 +11,22 @@ class App extends Component {
       { name: "Arbejas", price: 2500, img: "/productos/arbejas.jpg" },
       { name: "Lechuga", price: 500, img: "/productos/lechuga.jpg" },
     ],
-    carro: [
-      /* { name: "Tomate", price: 1500, img: "/productos/tomate.jpg", cantidad:1 } */
-    ],
+    carro: [],
   };
 
   agregarAlCarro = (producto) => {
+    const { carro } = this.state;
+    if (carro.find((x) => x.name === producto.name)) {
+      const newCarro = carro.map((x) =>
+        x.name === producto.name
+          ? {
+              ...x,
+              cantidad: x.cantidad + 1,
+            }
+          : x
+      );
+      return this.setState({ carro: newCarro });
+    }
     /* console.log(producto); */
     return this.setState({
       carro: this.state.carro.concat({ ...producto, cantidad: 1 }),
@@ -24,10 +34,10 @@ class App extends Component {
   };
 
   render() {
-    console.log(this.state.carro);
+    /*  console.log(this.state.carro); */
     return (
       <div>
-        <Navbar />
+        <Navbar carro={this.state.carro} />
         <Layout>
           <Title />
           <Productos
