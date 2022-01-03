4a2cac97f1818f715d0e7822eb8503bf4e8329cc
Diego <drbv27@gmail.com>
2022-01-03 08:30:07 -0500

    Bubblealert and adding to car

--- src/App.js
+++ src/App.js
@@ -11,15 +11,27 @@ class App extends Component {
       { name: "Arbejas", price: 2500, img: "/productos/arbejas.jpg" },
       { name: "Lechuga", price: 500, img: "/productos/lechuga.jpg" },
     ],
+    carro: [
+      /* { name: "Tomate", price: 1500, img: "/productos/tomate.jpg", cantidad:1 } */
+    ],
   };
+
+  agregarAlCarro = (producto) => {
+    /* console.log(producto); */
+    return this.setState({
+      carro: this.state.carro.concat({ ...producto, cantidad: 1 }),
+    });
+  };
+
   render() {
+    console.log(this.state.carro);
     return (
       <div>
         <Navbar />
         <Layout>
           <Title />
           <Productos
-            agregarAlCarro={() => console.log("Nohace nada")}
+            agregarAlCarro={this.agregarAlCarro}
             productos={this.state.productos}
           />
         </Layout>
--- src/components/Carro.js
+++ src/components/Carro.js
@@ -21,7 +21,7 @@ class Carro extends Component {
     return (
       <div>
         <span style={styles.bubble}>
-          <BubbleAlert />
+          <BubbleAlert value={11} />
         </span>
         <button style={styles.carro}>Carro</button>
       </div>
